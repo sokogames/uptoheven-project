61d4d5e6f14db8240dfa049627de63cc6f1fcd33
levangagnidze <levan.gagnidze@gmail.com>
2015-09-15 00:12:46 +0400

    დინამიური ობიექტების დამატება

---
+++ UpToHeven/Unity/Assets/Scenes/ChangeObjects.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeObjects : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		foreach (GameObject obj in GameObject.FindGameObjectsWithTag ("StaticObscale")) {
+		
+			if( hasChild(obj.transform, "futur_step")){
+				obj.GetComponent<BoxCollider>().isTrigger = true;
+				BoxCollider collider =  obj.AddComponent<BoxCollider>();
+				collider.center = new Vector3(0,-0.5f,0);
+				collider.size = new Vector3(1,1,1);
+				Debug.Log(obj.transform.parent.gameObject.name);
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	bool hasChild(Transform tr, string childName){
+	
+		foreach(Transform child in tr){
+		
+			if(child.gameObject.name == childName){
+				return true;
+			}
+		
+		}
+
+		return false;
+	
+	}
+}
