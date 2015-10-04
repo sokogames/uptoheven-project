feadf90a4709f27c147bd897d3e709a44f2bd035
levangagnidze <levan.gagnidze@gmail.com>
2015-10-04 22:21:18 +0400

    შუქი და კამერა
    გაშუქებისას არეკვლა აღარ მოხდება. ასევე კამერის მარცხნივ და მარჯვნივ
    მოძრაობა დაიხვეწა.

--- UpToHeven/Unity/Assets/Scripts/Camera/FollowPlayer.cs
+++ UpToHeven/Unity/Assets/Scripts/Camera/FollowPlayer.cs
@@ -5,6 +5,7 @@ public class FollowPlayer : MonoBehaviour {
 	public Player player;
 	public float lerpSpeed;
 	public Vector3 offset;
+	public Vector3 scale;
 
 	private Vector3 point;
 	// Use this for initialization
@@ -15,7 +16,7 @@ public class FollowPlayer : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		point = player.transform.position + offset;
+		point = Vector3.Scale(player.transform.position , scale) + offset;
 
 		Vector3 newPos = Vector3.Lerp (gameObject.transform.position
 		                               ,point
