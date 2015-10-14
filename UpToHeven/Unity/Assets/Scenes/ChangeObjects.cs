3e4142e685dc5919bf88f107f3ddc6db032c64cf
levangagnidze <levan.gagnidze@gmail.com>
2015-10-14 12:48:40 +0400

    future step 1 changed to future step 2
    future step 1 changed to future step 2

--- UpToHeven/Unity/Assets/Scenes/ChangeObjects.cs
+++ UpToHeven/Unity/Assets/Scenes/ChangeObjects.cs
@@ -3,17 +3,21 @@ using System.Collections;
 
 public class ChangeObjects : MonoBehaviour {
 
+	public GameObject newStep;
 	// Use this for initialization
 	void Start () {
 	
 		foreach (GameObject obj in GameObject.FindGameObjectsWithTag ("StaticObscale")) {
 		
 			if( hasChild(obj.transform, "futur_step")){
-				obj.GetComponent<BoxCollider>().isTrigger = true;
-				BoxCollider collider =  obj.AddComponent<BoxCollider>();
-				collider.center = new Vector3(0,-0.5f,0);
-				collider.size = new Vector3(1,1,1);
-				Debug.Log(obj.transform.parent.gameObject.name);
+				GameObject gameObject = (GameObject) Instantiate (newStep,obj.transform.position,obj.transform.rotation);
+				gameObject.name = obj.name;
+				gameObject.transform.parent = obj.transform.parent;
+				//obj.GetComponent<BoxCollider>().isTrigger = true;
+				//BoxCollider collider =  obj.AddComponent<BoxCollider>();
+				//collider.center = new Vector3(0,-0.5f,0);
+				//collider.size = new Vector3(1,1,1);
+				Destroy(obj);
 			}
 		}
 
