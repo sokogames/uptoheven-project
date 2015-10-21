d077d5ba87f0157b6b659d8769606e2a47056f81
levangagnidze <levan.gagnidze@gmail.com>
2015-10-21 23:30:58 +0400

    Landscape mode added

---
+++ UpToHeven/Unity/Assets/Scripts/Camera/AdjustAfterOrientation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdjustAfterOrientation : MonoBehaviour {
+
+	private Camera currentCamera;
+	private float landscapeSize;
+	public float portraitSize;
+	// Use this for initialization
+	void Start () {
+		currentCamera = GetComponent<Camera> ();
+		float scale = Mathf.Min((float)Screen.width,(float)Screen.height) / Mathf.Max((float)Screen.width,(float)Screen.height);
+		landscapeSize = portraitSize * scale;														
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isLandscape()) 															{
+			currentCamera.orthographicSize = landscapeSize;
+		} else {
+			currentCamera.orthographicSize = portraitSize;
+		}
+	}
+	bool isLandscape(){
+		return Input.deviceOrientation == DeviceOrientation.LandscapeLeft || 
+			Input.deviceOrientation == DeviceOrientation.LandscapeRight;
+	}
+}
