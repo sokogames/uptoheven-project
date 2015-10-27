f874272206e1f15e15ee9e6cfbc9fce24486243f
levangagnidze <levan.gagnidze@gmail.com>
2015-10-27 23:58:44 +0400

    Player movement bug fixing

--- UpToHeven/Unity/Assets/Scripts/Controller/Input/InputController.cs
+++ UpToHeven/Unity/Assets/Scripts/Controller/Input/InputController.cs
@@ -8,40 +8,47 @@ public class InputController : MonoBehaviour {
 	public ActionManager rotationActions;
 	public ActionManager jumpActions;
 
+	private bool previousTap;
+
 	// Use this for initialization
 	void Start () {
-		
+		previousTap = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// Update is called once per frame
-			InputCommand command = inputHandler.InputHandle();
+		
+		if (previousTap) {
+			jumpActions.AddAction(new JumpAction(actor));
+			previousTap = false;
+		}	
+
+		InputCommand command = inputHandler.InputHandle();
 			
-			switch (command) {
-			case InputCommand.Tap:
-				rotationActions.AddAction(new RotateAction(actor,RotateAction.ACTION_FACE_FORWARD));
-				jumpActions.AddAction(new JumpAction(actor));
-				break;
-			case InputCommand.Left: 
-				rotationActions.AddAction(new RotateAction(actor,RotateAction.ACTION_FACE_LEFT));
-				break;
-			case InputCommand.Right: 
-				rotationActions.AddAction(new RotateAction(actor,RotateAction.ACTION_FACE_RIGHT));
-				break;
-			case InputCommand.Up: 
-				rotationActions.AddAction(new RotateAction(actor,RotateAction.ACTION_FACE_FORWARD));
-				break;
-			case InputCommand.Down: 
-				rotationActions.AddAction(new RotateAction(actor,RotateAction.ACTION_FACE_BACKWARD));
-				break;
-			case InputCommand.Realese:
-				jumpActions.AddAction(new JumpAction(actor));
-				//actions.AddAction(new RotateAction(actions.gameObject,RotateAction.ACTION_FACE_FORWARD));
-				break;
-			default:
-				break;
-			}
-			
+		switch (command) {
+		case InputCommand.Tap:
+			rotationActions.AddAction(new RotateAction(actor,RotateAction.ACTION_FACE_FORWARD));
+			previousTap = true;
+			break;
+		case InputCommand.Left: 
+			rotationActions.AddAction(new RotateAction(actor,RotateAction.ACTION_FACE_LEFT));
+			break;
+		case InputCommand.Right: 
+			rotationActions.AddAction(new RotateAction(actor,RotateAction.ACTION_FACE_RIGHT));
+			break;
+		case InputCommand.Up: 
+			rotationActions.AddAction(new RotateAction(actor,RotateAction.ACTION_FACE_FORWARD));
+			break;
+		case InputCommand.Down: 
+			rotationActions.AddAction(new RotateAction(actor,RotateAction.ACTION_FACE_BACKWARD));
+			break;
+		case InputCommand.Realese:
+			jumpActions.AddAction(new JumpAction(actor));
+			//actions.AddAction(new RotateAction(actions.gameObject,RotateAction.ACTION_FACE_FORWARD));
+			break;
+		default:
+			break;
+		}
 	}
 }
