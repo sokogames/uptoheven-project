ead3de74245412787b99753be155d014a1cc51dd
levangagnidze <levan.gagnidze@gmail.com>
2016-01-26 18:01:20 +0400

    Tweening for main screen

--- UpToHeven/Unity/Assets/Scripts/UI/GamePanel.cs
+++ UpToHeven/Unity/Assets/Scripts/UI/GamePanel.cs
@@ -4,8 +4,12 @@ using UnityEngine.UI;
 
 public class GamePanel : MonoBehaviour {
 
+	const float buttonOffsetY = -40; 
+
 	public Player player;
 	private Text text;
+
+	private 
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +18,36 @@ public class GamePanel : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		text.text = Mathf.Max (1,(player.maxPosition)).ToString();
+		text.text = Mathf.Max (231,(player.maxPosition)).ToString();
+	}
+
+	public void ClickPlay(GameObject sender){
+		iTween.MoveBy (sender, new Vector3 (0, buttonOffsetY, 0), 0.1f);
+		Invoke ("StartGame", 0.5f);
+		Invoke ("Fade", 0.2f);
+	}
+	public void ClickPlayer(GameObject sender){
+		iTween.MoveBy (sender, new Vector3 (0, buttonOffsetY, 0), 0.1f);
+	}
+	public void ClickScore(GameObject sender){
+		iTween.MoveBy (sender, new Vector3 (0, buttonOffsetY, 0), 0.1f);
+	}
+	public void ClickSettings(GameObject sender){
+		iTween.MoveBy (sender, new Vector3 (0, buttonOffsetY, 0), 0.1f);
+	}
+	void StartGame(){
+		GameObject.Find ("_main").GetComponent<GameController> ().StartGame ();
+	}
+	void Fade(){
+			FadeAllChild(transform,0.5f);
+	}
+	void FadeAllChild(Transform parent, float duration){
+		foreach(Transform tr in parent){
+			Image img = tr.GetComponent<Image>();
+			if(img){
+				img.CrossFadeColor(new Color(255,255,255,0), duration, false,true);
+			}
+			FadeAllChild(tr,duration);
+		}
 	}
 }
